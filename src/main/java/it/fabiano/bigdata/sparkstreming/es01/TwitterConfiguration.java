3898ff1bee2f4ef6669312eef49fd567c2403d17
gaetanofabiano <fabiano.gaetano@gmail.com>
2020-07-13 10:41:10 +0200

    hiding keys

--- src/main/java/it/fabiano/bigdata/sparkstreming/es01/TwitterConfiguration.java
+++ src/main/java/it/fabiano/bigdata/sparkstreming/es01/TwitterConfiguration.java
@@ -2,10 +2,10 @@ package it.fabiano.bigdata.sparkstreming.es01;
 
 public class TwitterConfiguration {
 	//Credentials
-	public final static String consumerKey = "eD1fATQ0VtXCYEc3toSM1Xgng";
-	public final static String consumerSecret = "KbAuly1xcly55lIB7CShl0JcK0saDHOci5cH1p2X9vV8HTvpcy";
-	public final static String accessToken = "841603218-5S1KTpS6gOOcTPNeJMDqm2vtDRqGWGrX1aQfx2xM";
-	public final static String accessTokenSecret = "YnUnqen0Oy024gyyCQ8h8wloirZi2mxgKdLz9g1UhQ7oE";
+	public final static String consumerKey = "";
+	public final static String consumerSecret = "";
+	public final static String accessToken = "";
+	public final static String accessTokenSecret = "";
 	
 	static {
 		System.setProperty("twitter4j.oauth.consumerKey", consumerKey);
